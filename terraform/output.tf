7e4e6db7fd8ebd76bad6b18ddd93c39f6a7857c6
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2025-06-03 20:31:36 +0530

    Update output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "rds_endpoint" {
+  value = module.rds.rds_endpoint
+}
+
+output "rds_sg_id" {
+  value = module.vpc.rds_sg_id
+}

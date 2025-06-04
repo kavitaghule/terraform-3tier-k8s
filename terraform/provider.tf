912a8bc4c210f1d7ddf390fb149c09618d88077e
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2025-06-04 09:59:45 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,18 @@ provider "aws" {
     secret_key = data.vault_generic_secret.aws.data["secret_key"]
     access_key = data.vault_generic_secret.aws.data["access_key"]
 }
+provider "aws" {
+  region                      = var.region
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    rds = "http://localhost:4566"
+    eks = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
+}

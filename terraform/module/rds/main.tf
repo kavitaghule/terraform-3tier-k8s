5a7392e290e97324b29b9b8c4daaa8c86637754b
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2025-06-04 09:55:39 +0530

    Update main.tf

--- terraform/module/rds/main.tf
+++ terraform/module/rds/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_db_subnet_group" "main" {
   name       = "${var.environment}-rds-subnet-group"
   subnet_ids = var.private_subnets
@@ -17,4 +18,17 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks #_
+    cidr_blocks = var.allowed_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.environment}-rds-sg"
+    Environment = var.environment
+  }
